91e0bd99b6ee4afddd21365b1b6e8ff7907698b0
saed <khaleds@itu.edu.tr>
2018-07-11 18:26:15 +0300

    creating reciving messages

---
+++ app/src/main/java/com/safaorhan/reunion/adapter/ChatAdapter.java
@@ -0,0 +1,79 @@
+package com.safaorhan.reunion.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.safaorhan.reunion.R;
+import com.safaorhan.reunion.model.Message;
+import com.safaorhan.reunion.model.User;
+
+public class ChatAdapter extends FirestoreRecyclerAdapter<Message, ChatAdapter.ChatHolder> {
+    private static final String TAG = ChatAdapter.class.getSimpleName();
+
+    public ChatAdapter(@NonNull FirestoreRecyclerOptions<Message> options) {
+        super(options);
+    }
+
+    public static ChatAdapter get(DocumentReference conversationRef) {
+        Query query = FirebaseFirestore.getInstance()
+                .collection("messages")
+                .whereEqualTo("conversation", conversationRef)
+                .orderBy("sentAt")
+                .limit(50);
+
+        FirestoreRecyclerOptions<Message> options = new FirestoreRecyclerOptions.Builder<Message>()
+                .setQuery(query, Message.class)
+                .build();
+
+        return new ChatAdapter(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ChatHolder holder, int position, @NonNull Message message) {
+        holder.bind(message);
+    }
+
+    @NonNull
+    @Override
+    public ChatHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat, parent, false);
+        return new ChatHolder(itemView);
+    }
+
+    public class ChatHolder extends RecyclerView.ViewHolder {
+
+        View itemView;
+        TextView nameText;
+        TextView messageText;
+
+        public ChatHolder(View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            nameText = itemView.findViewById(R.id.name_text);
+            messageText = itemView.findViewById(R.id.message_text);
+        }
+
+        public void bind(final Message message) {
+
+            message.getFrom().get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    User user = documentSnapshot.toObject(User.class);
+                    nameText.setText(user.getName());
+                }
+            });
+
+            messageText.setText(message.getText());
+        }
+    }
+}
