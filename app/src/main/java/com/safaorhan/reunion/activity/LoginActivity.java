524c584b9333a4031fe8cb18862a6e8892ac363a
hayaboudann <hayachandot@gmail.com>
2018-07-12 13:16:53 +0300

    fixed the issue with log_in keyboard

--- app/src/main/java/com/safaorhan/reunion/activity/LoginActivity.java
+++ app/src/main/java/com/safaorhan/reunion/activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -27,6 +28,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_login);
 
         emailEdit = findViewById(R.id.emailEdit);
